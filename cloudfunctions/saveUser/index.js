2fb2280d4ee51c35dfd5e143c6129eb6ab19e8da
lzx916 <770914373@qq.com>
2023-03-28 20:14:45 +0800

    添加homelzx首页

---
+++ cloudfunctions/saveUser/index.js
@@ -0,0 +1,32 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  // env: cloud.DYNAMIC_CURRENT_ENV
+  env: "cloud1-6gb3kr0dd5a291f1"
+})
+const db = cloud.database()
+const _ = db.command
+// 云函数入口函数
+exports.main = async(event, context) => {
+  try {
+    return await db.collection('user').add({
+      data: {
+        _id: event.openid,
+        loginTime: event.loginTime,
+        lastLoginTime: event.lastLoginTime,
+        avatarUrl: event.avatarUrl,
+        city: event.city,
+        country: event.country,
+        gender: event.gender,
+        language: event.language,
+        nickName: event.nickName,
+        province: event.province,
+      }
+    }).then(res => {
+      return res;
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
---
+++ cloudfunctions/updateUsers/index.js
@@ -0,0 +1,22 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  // env: cloud.DYNAMIC_CURRENT_ENV
+  env: "cloud1-6gb3kr0dd5a291f1"
+})
+const db = cloud.database()
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    return await db.collection('user').doc(event._id).update({
+      data: {
+        lastLoginTime: event.lastLoginTime
+      }
+    }).then(res => {
+      return res;
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
